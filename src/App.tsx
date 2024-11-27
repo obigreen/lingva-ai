e5a92b080d4f13fd0f2fda2288908836a70f5d25
Sergey Budyakin <youshtuchka@gmail.com>
2024-11-27 11:33:29 +0300

    in only words and phrases, and all

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,18 @@ import React from "react";
 import './App.css';
 import {English} from "./english/English";
 import {S} from "./styles/BlockStyles";
-import {Header} from "./english/header/Header";
-import {Footer} from "./english/footer/Footer";
 
 
 const App = () => {
     return (
         <S.Main>
             <S.GlobalWrapper>
-                <Header/>
                 <S.Container>
-                    <S.MainTitle>English words</S.MainTitle>
+                    <S.MainTitle>English words and phrases</S.MainTitle>
                     <S.NoteBlock>
                         <English/>
                     </S.NoteBlock>
                 </S.Container>
-                <Footer/>
             </S.GlobalWrapper>
         </S.Main>
     );
