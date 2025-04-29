832ec8aacb0845210705c039ac8dcae3c63b3cbd
Sergey Budyakin <youshtuchka@gmail.com>
2025-04-29 23:38:17 +0300

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import './App.css';
 import {English} from "./english/English";
 import {S} from "./styles/BlockStyles";
+import styled from "styled-components";
 
 
 const App = () => {
     return (
         <S.Main>
             <S.GlobalWrapper>
+                <header>
+                    <Container>
+                        <div>
+                            <a href="">
+                                <img src="" alt=""/>
+                            </a>
+                        </div>
+                        <nav>
+                            <a href="">Login</a>
+                        </nav>
+                    </Container>
+                </header>
                 <S.Container>
                     <S.MainTitle>English words and phrases</S.MainTitle>
                     <S.NoteBlock>
@@ -22,3 +35,12 @@ const App = () => {
 
 export default App;
 
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 0 auto;
+    max-width: 900px;
+    padding: 15px;
+    color: white;
+`;
